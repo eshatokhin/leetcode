b9db8caa725852e6b4b7034aa75b9585e95281bd
Yevhen Shatokhin <argelios@gmail.com>
2021-02-11 21:30:54 +0200

    13 Roman to Integer

--- index.js
+++ index.js
@@ -54,9 +54,47 @@ var isPalindrome = function(x) {
  */
 var romanToInt = function(s) {
 	var retVal = 0;
+	var re = /^[IVXLCDM]*$/gi;
+	if (!re.test(s))
+	{
+		console.log("argument must contain only characters 'I', 'V', 'X', 'L', 'C', 'D', 'M'");
+	}
+
+	var aRom = {
+		"I":1,
+		"V":5,
+		"X":10,
+		"L":50,
+		"C":100,
+		"D":500,
+		"M":1000,
+		"IV":4,
+		"IX":9,
+		"XL":40,
+		"XC":90,
+		"CD":400,
+		"CM":900,
+	};
+
+	for (var i=0; i<s.length; i++)
+	{
+		var char = s[i];
+		var nextChar = s[i+1];
+
+		if (aRom[char+nextChar])
+		{
+			retVal += aRom[char+nextChar];
+			i++;
+		}
+		else
+		{
+			retVal += aRom[char];
+		}
+	}
 
 	console.log(retVal);
-	return retVal;
+	//return retVal;
 };
 
-romanToInt("III");
+romanToInt("MCMXCIV");
+
